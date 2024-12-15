8b7f9c48abaef65d592c4ee338d3e54733b51791
hoangphuc1501 <katakurilancer.com>
2024-12-15 15:16:17 +0700

    lọc theo trạng thái

--- controllers/client/task.controller.ts
+++ controllers/client/task.controller.ts
@@ -3,18 +3,25 @@ import { Request, Response } from "express";
 import { Task } from "../../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
-        deleted: false
-    })
+    const find = {
+        deleted: false,
+    };
+    // lọc theo trạng thái
+    if (req.query.status) {
+        find["status"] = req.query.status;
+    }
+    // hết lọc theo trạng thái
+
+    const tasks = await Task.find(find);
     res.json(tasks);
-}
+};
 
 export const detail = async (req: Request, res: Response) => {
     const id: string = req.params.id;
 
     const task = await Task.findOne({
         _id: id,
-        deleted: false
-    })
+        deleted: false,
+    });
     res.json(task);
-}
+};
